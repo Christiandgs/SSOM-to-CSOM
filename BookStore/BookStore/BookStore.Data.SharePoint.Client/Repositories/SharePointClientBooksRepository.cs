06b3c8bbe4fca215ba10201ecd5c7096d0e2d289
chru <christian.ruiz@spenta.es>
2016-01-30 00:24:42 +0100

    Client Book Repository implemented

--- BookStore/BookStore/BookStore.Data.SharePoint.Client/Repositories/SharePointClientBooksRepository.cs
+++ BookStore/BookStore/BookStore.Data.SharePoint.Client/Repositories/SharePointClientBooksRepository.cs
@@ -1,10 +1,12 @@
 ﻿using BookStore.Data.Model;
 using BookStore.Data.Repositories;
+using Microsoft.SharePoint.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace BookStore.Data.SharePoint.Client.Repositories
 {
@@ -14,8 +16,41 @@ namespace BookStore.Data.SharePoint.Client.Repositories
 
         public List<Book> GetByCategory(int categoryId)
         {
-            //TODO: Implement this method using CSOM
-            return new List<Book>();
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext.Current);
+            var clientContext = spContext.CreateUserClientContextForSPHost();
+
+            var web = clientContext.Web;
+
+            var booksList = web.Lists.GetByTitle(ListName);
+
+            var query = new CamlQuery();
+            query.ViewXml = string.Format(
+@"<View>
+    <Query>
+        <Where>
+            <Eq>
+                <FieldRef Name='Category' LookupId='TRUE' />
+                <Value Type='Lookup'>{0}</Value>
+            </Eq>
+        </Where>
+    </Query>
+</View>", categoryId);
+
+            var items = booksList.GetItems(query);
+
+            clientContext.Load(items);
+            clientContext.ExecuteQueryRetry();
+
+            var booksInCategory = items.OfType<ListItem>().Select(it => new Book
+            {
+                Id = it.Id,
+                Title = it["Title"] as string,
+                Description = it["Description"] as string,
+                ImageUrl = ((FieldUrlValue)it["Image"]).Url,
+                CategoryId = categoryId
+            }).ToList();
+
+            return booksInCategory;
         }
     }
 }
